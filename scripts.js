18f5d2d4b2b1ca247de45cb1be68c147f935dc0b
Paulo <pc5783346@gmail.com>
2022-04-06 23:17:29 -0300

    add da função sort

--- scripts.js
+++ scripts.js
@@ -12,13 +12,20 @@ while (numcards%2 !== 0 || numcards < 4 || numcards > 14){
 for(let i=0; i<2; i++){
     for(let j=1; j<=numcards/2; j++){
         setCards[cont] = `<div class="card" onclick="turn_card(this)"><img src="./images/front.png"/><img class="hidden" src="./images/parrot_${j}.gif"</div>`;
-        console.log(setCards[cont]);
+        console.log(setCards[cont]+" criado");
         cont++;
     }
 }
 
+
 for(let i=0; i<setCards.length; i++){
+    setCards.sort(comparador);
     document.querySelector(".conteiner").innerHTML += setCards[i];
+    console.log(setCards[i]+" embaralhado");
+}
+
+function comparador(){
+    return Math.random() - 0.5;
 }
 
 function turn_card(parrot){
